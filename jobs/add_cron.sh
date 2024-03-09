72e7b1a23b079001f976312c95d259bf5cbb5f2d
cwadven <cwadven@naver.com>
2024-03-09 10:31:00 +0900

    Add cron

--- jobs/add_cron.sh
+++ jobs/add_cron.sh
@@ -1,2 +1,7 @@
+if [ -e "$MY_PROJECT_DIRECTORY/command.cron" ];
+then
 cat $MY_PROJECT_DIRECTORY/command.cron | sudo crontab -
 sudo /etc/init.d/cron reload
+else
+echo "command.cron file does not exist"
+fi
