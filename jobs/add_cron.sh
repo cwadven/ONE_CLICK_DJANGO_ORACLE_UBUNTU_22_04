77ac6677444a88f27120100b0fdcdffc7cf3c5b0
이창우 <cwadven@naver.com>
2024-03-24 15:54:20 +0900

    Update add_cron.sh

--- jobs/add_cron.sh
+++ jobs/add_cron.sh
@@ -1,7 +1,8 @@
-if [ -e "$MY_PROJECT_DIRECTORY/command.cron" ];
+if [ -e "$MY_PROJECT_DIRECTORY/crontab.j2" ];
 then
+. $MY_PROJECT_DIRECTORY/venv/bin/activate && cd $MY_PROJECT_DIRECTORY && fab2 update-crontab
 cat $MY_PROJECT_DIRECTORY/command.cron | sudo crontab -
 sudo /etc/init.d/cron reload
 else
-echo "command.cron file does not exist"
+echo "crontab.j2 file does not exist"
 fi
